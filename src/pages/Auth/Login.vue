57da024f54cffcd4908a31be1bb4db69de423f49
victornooblath <Paulov2001@outlook.com>
2022-06-02 20:02:08 -0300

    feat: functional login and register

--- src/pages/Auth/Login.vue
+++ src/pages/Auth/Login.vue
@@ -2,21 +2,54 @@
   <q-page padding>
     <q-card>
       <q-card-section>
-        <h1 @click="$router.push({ name: 'register' })">Register</h1>
-      </q-card-section>
-      <q-card-section>
+        <div style="text-align: center">
+          <h5 style="color: #f35b04; margin-bottom: 0px">
+            Bem vindo(a) ao Save Eats
+          </h5>
+          <span style="color: blue; margin-top: 0px"
+            >Faça o login para ter acesso as mais novas promoções perto de
+            você!</span
+          >
+        </div>
         <q-form @submit="logIn">
-          <q-input type="email" v-model="user.email" />
-          <q-input type="password" v-model="user.password" />
-          <q-btn :loading="loading" label="Submit" type="submit" />
+          <q-input
+            label="Email"
+            type="email"
+            v-model="user.email"
+            :rules="[(val) => !!val || '* Este campo é obrigatório']"
+            color="orange"
+          />
+          <q-input
+            label="Senha"
+            type="password"
+            v-model="user.password"
+            :rules="[(val) => !!val || '* Este campo é obrigatório']"
+            color="orange"
+          />
+          <div style="text-align: center; margin-top: 20px">
+            <q-btn
+              color="deep-orange"
+              rounded
+              glossy
+              :loading="loading"
+              label="Logar"
+              type="submit"
+            />
+          </div>
         </q-form>
       </q-card-section>
     </q-card>
+    <p style="text-align: center; margin-top: 10px">
+      Não possui conta?<a @click="$router.push({ name: 'register' })">
+        clique aqui!</a
+      >
+    </p>
   </q-page>
 </template>
 
 <script>
 import { signInWithEmailAndPassword } from "firebase/auth";
+import { mapMutations } from "vuex";
 export default {
   data() {
     return {
@@ -28,26 +61,44 @@ export default {
     };
   },
   methods: {
+    ...mapMutations({
+      setUserMutation: "user/setUser",
+    }),
+
     async logIn() {
       this.loading = true;
       try {
-        const user = await signInWithEmailAndPassword(
+        const {
+          user: { uid, email },
+        } = await signInWithEmailAndPassword(
           this.$auth,
           this.user.email,
           this.user.password
         );
-        console.log(user);
-        
+        this.$q.notify({
+          color: "secondary",
+          message: "Bem vindo de volta!",
+          icon: "thumb_up",
+          position: "top",
+        });
+        this.setUserMutation({ uid, email });
+        this.$q.localStorage.set("userId", uid);
+        this.$router.push({ name: "home" });
       } catch (err) {
         console.error(err);
+        this.$q.notify({
+          color: "negative",
+          message: `ERROR!: ${err.code}`,
+          icon: "warning",
+          position: "top",
+        });
       } finally {
         this.loading = false;
-        this.user.email = "";
-        this.user.password = "";
-        console.log(`Logged with ${this.user}`)
+        // this.user.email = "";
+        // this.user.password = "";
+        console.log(`Logged with ${this.user}`);
       }
     },
   },
 };
 </script>
-
--- src/pages/Auth/Register.vue
+++ src/pages/Auth/Register.vue
@@ -1,23 +1,67 @@
 <template>
   <q-page padding>
     <q-card>
-      <q-card-section>
-        <h1 @click="$router.push({ name: 'login' })">Login</h1>
+      <q-card-section class="align-items-center">
+        <div style="text-align: center">
+          <img src="~assets/grupo3.png" :ratio="1" />
+          <h4 style="color: #f35b04; margin-bottom: 0px">olá!</h4>
+          <span style="color: blue; margin-top: 0px"
+            >Seja bem vindo ao Save Eats, o aplicativo que permite que você
+            economize enquanto previne desperdício</span
+          >
+        </div>
       </q-card-section>
       <q-card-section>
         <q-form @submit="onSubmit">
-          <q-input type="email" v-model="user.email" />
-          <q-input type="password" v-model="user.password" />
-          <q-btn :loading="loading" label="Submit" type="submit" />
+          <q-input
+            label="Email"
+            type="email"
+            v-model="user.email"
+            :rules="[
+              (val) => !!val || '* Este campo é obrigatório',
+              isValidEmail,
+            ]"
+            color="orange"
+            clearable
+          />
+          <q-input
+            label="Senha"
+            type="password"
+            v-model="user.password"
+            :rules="[
+              (val) => !!val || '* Este campo é obrigatório',
+              (val) =>
+                val.length >= 6 ||
+                '* A senha precisa conter no minimo 6 caracteres',
+            ]"
+            color="orange"
+            clearable
+          />
+          <div style="text-align: center; margin-top: 20px">
+            <q-btn
+              color="deep-orange"
+              rounded
+              glossy
+              :loading="loading"
+              label="Cadastrar"
+              type="submit"
+            />
+          </div>
         </q-form>
       </q-card-section>
     </q-card>
+    <p style="text-align: center; margin-top: 10px">
+      Ja possui conta?<a @click="$router.push({ name: 'login' })">
+        clique aqui!</a
+      >
+    </p>
   </q-page>
 </template>
 
 <script>
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { addDoc, collection } from "firebase/firestore";
+import { Notify } from "quasar";
 
 export default {
   data() {
@@ -49,15 +93,33 @@ export default {
           this.user.password
         );
         this.setUserDoc(uid);
+        Notify.create({
+          color: "secondary",
+          message: "Usuário Cadastrado!",
+          icon: "thumb_up",
+          position: "top",
+        });
         console.log(uid);
       } catch (err) {
         console.error(err);
+        Notify.create({
+          color: "negative",
+          message: `ERROR!: ${err.code}`,
+          icon: "warning",
+          position: "top",
+        });
       } finally {
         this.loading = false;
         this.user.email = "";
         this.user.password = "";
       }
     },
+    isValidEmail(val) {
+      const emailPattern =
+        /^(?=[a-zA-Z0-9@._%+-]{6,254}$)[a-zA-Z0-9._%+-]{1,64}@(?:[a-zA-Z0-9-]{1,63}\.){1,8}[a-zA-Z]{2,63}$/;
+      return emailPattern.test(val) || "E-mail inválido";
+    },
   },
 };
 </script>
+<style scoped></style>
